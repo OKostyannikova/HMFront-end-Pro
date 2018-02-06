2efaf653aa6fa6614f878c15d7c6339cde4b2742
Oksana Kostyannikova <Oksana@BoG.local>
2018-02-06 12:46:00 +0200

    requests v3

--- lesson14/assets/request.js
+++ lesson14/assets/request.js
@@ -1,22 +1,24 @@
 var Request = (function () {
+    var quary = function (url, string) {
+        return new Promise(function (res, rej) {
+            var xhr = new XMLHttpRequest();
+            xhr.open("post", url, true);
+            xhr.setRequestHeader("Content-type", "application/json");
+            xhr.addEventListener("load", function (data) {
+                var json = JSON.parse(data.target.responseText);
+                res(json);
+            })
+            xhr.send(JSON.stringify({ key: string }));
+        })
+    };
+
     function Request(url) {
         this.url = url;
-        this.list;
-    }
-    Request.prototype.ask = function (data) {
-        var self = this;
-        var xhr = new XMLHttpRequest();
-        xhr.open("post", this.url, true);
-        xhr.setRequestHeader("Content-type", "application/json");
-        xhr.addEventListener("load", function (data) {
-            var json = JSON.parse(data.target.responseText);
-            self.list = json;
-            console.log(self.list);
-        })
-        xhr.send(JSON.stringify(data));
-        console.log("Request sent...");
-    }
+    };
 
+    Request.prototype.ask = function (string) {
+        return quary(this.url, string);
+    };
     return Request;
 
 }());
